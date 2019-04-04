42f1bd8ee8650438e6bb8642541bf6e554158b6d
Lybeck Hansen <jorgen.lybeck-hansen@capgemini.com>
2019-04-04 13:44:16 +0200

    fix netlify toml file and 2 small build bugs

--- gatsby-client/src/pages/index.js
+++ gatsby-client/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import lax from 'lax.js'
 import { graphql } from "gatsby"
 import BlockContent from "@sanity/block-content-to-react";
 import Layout from "../components/Layout/Layout"
@@ -8,24 +7,12 @@ import * as Styled from "../components/IndexComponent/styles"
 import Particles from 'react-particles-js';
 import {getParticles} from "../particles-setup";
 
-lax.setup()
 // https://github.com/alexfoxy/laxxx/blob/master/README.md#supported-presets
-document.addEventListener('scroll', function (x) {
-  lax.update(window.scrollY)
-}, false)
-
-lax.update(window.scrollY)
 
 const IndexPage = ({ data }) => {
   const [particles, addParticles] = React.useState(100);
   const titleRef = React.createRef();
 
-  React.useEffect(() => {
-    if (titleRef.current) {
-      lax.addElement(titleRef.current)
-    }
-
-  }, [])
   
 
   return (
@@ -33,7 +20,7 @@ const IndexPage = ({ data }) => {
       <SEO title="Jørgen Lybeck Hansen" />
       <Styled.SplashScreenWrapper onClick={() => addParticles(particles + 5)}>
       <Particles style={{position: "absolute", top: 0, left: 0}} params={getParticles(particles)}/>
-        <Styled.SplashScreenTitle fontSize="5rem" ref={titleRef} data-lax-rotate data-lax-preset="fadeInOut">
+        <Styled.SplashScreenTitle fontSize="5rem" ref={titleRef}>
           Jørgen<br/>Lybeck<br/>Hansen
           </Styled.SplashScreenTitle>
         <Styled.SplashScreenArrow />
